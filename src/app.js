538187168b7b1908eca1c78b8577adcc74a0b590
Peter Schrammel <peter.schrammel@gmx.de>
2019-01-27 21:57:37 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+//middleware
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express();
+//const { applyMiddleware } = require('graphql-middleware');
+import server from './server';
+import models from './models';
+
+async function start(listen) {
+    // Make sure the database tables are up to date
+    //    await models.sequelize.authenticate();
+    var options = {
+        port: 3000,
+    }
+    // Start the GraphQL server
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(cors());
+    server.applyMiddleware({ app });
+    await models.User.setup();
+//    app.get('/api/', function (req, res) {
+//        res.send('GET request to homepage');
+//    });
+    app.get('/', (req,res) => {
+       res.send(JSON.stringify({ "success": true}))
+    })
+    
+    if (listen) {
+        app.listen(options,() => {
+            console.log(`Server is running on localhost`);
+        })
+    }
+
+}
+
+export default {
+    start,
+    models,
+}
