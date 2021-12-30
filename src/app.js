2bac75c9f67786022c77ccb8ca1801c9e623626a
Peter Schrammel <peter.schrammel@metoda.com>
2021-12-30 22:09:35 +0000

    patching works

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const _ = require('lodash');
+
 const app = express();
 //const { applyMiddleware } = require('graphql-middleware');
 //import server from './server';
@@ -28,6 +30,11 @@ Backends.register(require('./backends/mirroredLocal'));
 
 const uiTarget = process.env.UI_URL+'ui/'
 
+const jsonParser = bodyParser.json( { type: (req) => { 
+      console.log('headers:', req.headers['content-type'].match(/json/) )
+      return req.headers['content-type'].match(/json/)
+    }})
+    
 async function start(listen) {
     // Make sure the database tables are up to date
     //    await models.sequelize.authenticate();
@@ -35,10 +42,11 @@ async function start(listen) {
         port: 3000,
     }
 
-    app.use(logger('dev'));
+    app.use(logger('dev'))
     app.use(authMiddleware)
+    
+    
     // when adding something here don't forget to escape from the jdav servers below
-
     app.options('/ui', (req, res) => {
         console.log("Special ui options")
         res.end()
@@ -70,6 +78,62 @@ async function start(listen) {
             keyForAttribute: 'underscore_case',
         });
         var serialized = Serializer.serialize(clients);
+        res.send(JSON.stringify(serialized))
+    })
+
+    app.get('/api/v1/clients/:id', async (req, res) => {
+        console.log("Special clients")
+        var client = await models.Client.findOne({
+          where: {
+            user_id: 6,
+            id: req.params.id
+          }
+        })
+        
+        var Serializer = new JSONAPISerializer('clients', {
+            attributes: ['name','identifier','description','created_at', 'updated_at'],
+            keyForAttribute: 'underscore_case',
+        });
+        var serialized = Serializer.serialize(client);
+        res.send(JSON.stringify(serialized))
+    })
+
+    app.patch('/api/v1/clients/:id',  jsonParser, async (req, res) => {
+        console.log("Special clients", req.params.id)
+        
+        var client = await models.Client.findOne({
+          where: {
+            user_id: 6,
+            id: req.params.id
+          }
+        })
+        
+        changedAttributes = []
+        if (req.body && req.body.data && req.body.data.attributes) {
+          changedAttributes = Object.keys(req.body.data.attributes)
+          console.log("Changed attributes:", changedAttributes)
+        }
+        
+        
+        var Deserializer = new JSONAPIDeserializer('clients', {
+            attributes: _.intersection(['name','description'],changedAttributes),
+            keyForAttribute: 'underscore_case',
+        });
+        
+        var deserialized = await Deserializer.deserialize(req.body);
+        console.log("Deserialized:", deserialized)
+        Object.keys(deserialized).forEach( (key) => {
+          if (key == 'id') return
+          client[key] = deserialized[key]
+        })
+        
+        await client.save()
+
+        var Serializer = new JSONAPISerializer('clients', {
+            attributes: ['name','identifier','description','created_at', 'updated_at'],
+            keyForAttribute: 'underscore_case',
+        });
+        var serialized = Serializer.serialize(client);
         res.send(JSON.stringify(serialized))
     })
 
@@ -113,26 +177,26 @@ async function start(listen) {
 //        console.log("listeners:",listeners);
         httpServer.removeAllListeners("request");
         httpServer.addListener("request", function(req, resp) {
-            req.pause()
             var path = Url.parse(req.url).pathname;
             console.log("Path", path, httpServer.listeners.length);
             if (path.charAt(path.length - 1) != "/")
                 path = path + "/";
-            if (path == '/' && req.method == 'GET') {
+            if (path == '/' && req.method == 'GET') {    // the special path for root
                 console.log(`Special route: '${path}'`)
                 for (var i = 0, len = listeners.length; i < len; ++i)
                     listeners[i].call(httpServer, req, resp);
-                console.log("Response:",resp.statusCode)
+                console.log("Response Home:",resp.statusCode)
             } else if (path.match(/^\/api\/|^\/ep\//)) { // add non jsdav paths here
                 console.log(`Special route: '${path}'`)
                 for (var i = 0, len = listeners.length; i < len; ++i)
-                    listeners[i].call(httpServer, req, resp);
-                console.log("Response:",resp.statusCode)
+                   listeners[i].call(httpServer, req, resp);
+                console.log("Response API:",resp.statusCode)
             } else {
+                req.pause() //this is a strange hack 
                 jsdavServer.exec(req, resp);
             }
 
-        });
+        })
     }
 }
 
